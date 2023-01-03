21ec6922be5288959976d405895ff86f2383c2ae
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-01-03 10:55:41 +0530

    Create Stack.h

---
+++ Memory/Stack.h
@@ -0,0 +1,21 @@
+//This file represents the implementation of during Csq4's runtime.
+#if !defined(PROGRAM_RUNTIME_STACK_CSQ4)
+#define PROGRAM_RUNTIME_STACK_CSQ4
+#include "../libs/utils/stringlib.h"
+#include "../libs/utils/dict.h"
+namespace Stack{
+    //This represents the code of functions from different scopes.
+    dict<str,str> Function;
+    //This represents the imports in the code.
+    array<str> Import;
+    //This stack represents the names of variables.
+    array<str> Variables;
+}
+namespace Scope{
+    //This dict represents the function's scope
+    dict<str,array<str>> Function;
+    //This dict represents the variable's scope
+    dict<str,array<str>> Variable;
+}
+
+#endif // PROGRAM_RUNTIME_STACK_CSQ4
