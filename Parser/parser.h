72734f6052f0eafb402bad7a93ab4d562c89bc9f
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-01-22 12:27:44 +0530

    Update parser.h

--- Parser/parser.h
+++ Parser/parser.h
@@ -143,6 +143,7 @@ str Rep(str s){
     code = replaceStr(code.Str,"< =","<=");
     code = replaceStr(code.Str,"- >","->");
     code = replaceStr(code.Str,"& &","&&");
+    code = replaceStr(code.Str," . ",".");
     return code;
 }
 bool CheckVariableAssignment(array<str> tokens){
