28f3de313c8c43e7d05dccd42f108d6e50e2de94
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-01-27 11:36:13 +0530

    Update builtins.h

--- Builtins/builtins.h
+++ Builtins/builtins.h
@@ -85,4 +85,71 @@ class i32{
             return state;
         }
 };
+
+class i64{
+    public:
+        int64_t val;
+        i64(long int n){
+            val = n;
+        }
+        i64(const i64& n){
+            val = n.val;
+        }
+        i64(){}
+       
+        auto op_add( SmartPointer<i64> v1, SmartPointer<i64> v2){
+            return SmartPointer<i64>((v1->val)+(v2->val));
+        }
+        auto op_sub( SmartPointer<i64> v1, SmartPointer<i64> v2){
+            return SmartPointer<i64>((v1->val)-(v2->val));
+        }
+        auto op_mul( SmartPointer<i64> v1, SmartPointer<i64> v2){
+            return SmartPointer<i64>((v1->val)*(v2->val));
+        }
+        auto op_div( SmartPointer<i64> v1, SmartPointer<i64> v2){
+            return SmartPointer<i64>((v1->val)/(v2->val));
+        }
+        auto op_mod( SmartPointer<i64> v1, SmartPointer<i64> v2){
+            return SmartPointer<i64>((v1->val)%(v2->val));
+        }
+        auto op_equal( SmartPointer<i64> v1, SmartPointer<i64> v2){
+            bool state = false;
+            if(v1->val == v2->val)
+                state = true;
+            return state;
+        }
+        auto op_notEqual( SmartPointer<i64> v1, SmartPointer<i64> v2){
+            bool state = false;
+            if(v1->val != v2->val)
+                state = true;
+            return state;
+        }
+        auto op_lesser( SmartPointer<i64> v1, SmartPointer<i64> v2){
+            bool state = false;
+            if(v1->val < v2->val)
+                state = true;
+            return state;
+        }
+        auto op_greater( SmartPointer<i64> v1, SmartPointer<i64> v2){
+            bool state = false;
+            if(v1->val > v2->val)
+                state = true;
+            return state;
+        }
+        auto op_greaterEqual( SmartPointer<i64> v1, SmartPointer<i64> v2){
+            bool state = false;
+            if(v1->val >= v2->val)
+                state = true;
+            return state;
+        }
+        auto op_lesserEqual( SmartPointer<i64> v1, SmartPointer<i64> v2){
+            bool state = false;
+            if(v1->val <= v2->val)
+                state = true;
+            return state;
+        }
+};
+
+
+
 #endif // BUILTINS_CSQ4
