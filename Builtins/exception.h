ad2761637d90e5693def583482224e9d28e82607
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-01-27 12:18:34 +0530

    Update exception.h

--- Builtins/exception.h
+++ Builtins/exception.h
@@ -6,5 +6,9 @@ void Trackback(){
     exit(EXIT_FAILURE);
 }
 
+void MemoryOverflowException(){
+    printf("Trackback:\n an object is trying to use memory which it doesn't owns\n");
+    Trackback();
+}
 
 #endif // 
