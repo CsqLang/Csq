dea34810e6e136f83fde591f353694eb8718b42a
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-02-06 15:56:38 +0530

    Update Reference_Counter.h

--- Memory/Reference_Counter.h
+++ Memory/Reference_Counter.h
@@ -30,6 +30,7 @@ private:
     ReferenceCounter *refCounter;
 
 public:
+    ref(){}
     ref(T *ptr) : ptr(ptr), refCounter(new ReferenceCounter()) {}
     ref(const ref<T> &sp) :
         ptr(sp.ptr), refCounter(sp.refCounter) {
@@ -75,17 +76,39 @@ public:
         refCounter = new ReferenceCounter();
     }
     ref<T> operator+(const ref<T> &rhs) {
-        return ref<T>(new T((this->op_add(new T(*ptr),new T(*rhs)))));
+        return (((this->op_add(new T(*ptr),new T(*rhs)))));
     }
     ref<T> operator-(const ref<T> &rhs) {
-        return ref<T>(new T((this->op_sub(new T(*ptr),new T(*rhs)))));
+        return (((this->op_sub(new T(*ptr),new T(*rhs)))));
     }
     ref<T> operator*(const ref<T> &rhs) {
-        return ref<T>(new T((this->op_mul(new T(*ptr),new T(*rhs)))));
+        return (this->op_mul(new T(*ptr),new T(*rhs)));
     }
     ref<T> operator/(const ref<T> &rhs) {
-        return ref<T>(new T((this->op_div(new T(*ptr),new T(*rhs)))));
+        return (this->op_div(new T(*ptr),new T(*rhs)));
     }
+    auto operator%(ref<T> op){
+      return this->op_mod(new T(*ptr),new T(*op));
+    }
+    auto operator>(ref<T> op){
+      return this->op_greater(new T(*ptr),new T(*op));
+    }
+    auto operator<(ref<T> op){
+      return this->op_lesser(new T(*ptr),new T(*op));
+    }
+    auto operator>=(ref<T> op){
+      return this->op_greaterEqual(new T(*ptr),new T(*op));
+    }
+    auto operator<=(ref<T> op){
+      return this->op_lesserEqual(new T(*ptr),new T(*op));
+    }
+    auto operator !=(ref<T> op){
+      return this->op_notEqual(new T(*ptr),new T(*op));
+    }
+    auto operator ==(ref<T> op){
+      return this->op_equal(new T(*ptr),new T(*op));
+    }
+    auto operator[](int index);
 };
 
 #endif // REFERENCECOUNT_H_CSQ4
