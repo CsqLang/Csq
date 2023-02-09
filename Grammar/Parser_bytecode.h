b27d047ceeacbb76c56e986fe14e3d497c9dc6db
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-02-09 11:34:17 +0530

    Create Parser_bytecode.h

---
+++ Grammar/Parser_bytecode.h
@@ -0,0 +1,11 @@
+#if !defined(PARSER_BYTECODE_CSQ4)
+#define PARSER_BYTECODE_CSQ4
+
+#include "../libs/utils/stringlib.h"
+
+//Bytecode variable representation
+str REFERENCE = "REFERENCE(";
+str FORREF = "FORREF(";
+
+
+#endif // PARSER_BYTECODE_CSQ4
