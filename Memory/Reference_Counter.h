81c3724680467e4cbc25f489846e4ea37aa3c546
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-02-10 19:53:10 +0530

    Update Reference_Counter.h

--- Memory/Reference_Counter.h
+++ Memory/Reference_Counter.h
@@ -2,10 +2,11 @@
 #define REFERENCECOUNT_H_CSQ4
 #include <stdio.h>
 #include <stdlib.h>
+#include <atomic>
 
 class ReferenceCounter{
 private:
-    int count;
+    std::atomic<int> count;
 
 public:
     ReferenceCounter() : count(1) {}
@@ -19,20 +20,19 @@ public:
     }
 
     int getCount() const {
-        return count;
+        return count.load();
     }
 };
 
 template <typename T>
-class ref  : T {
+class ref : T{
 private:
     T *ptr;
     ReferenceCounter *refCounter;
-
 public:
-    ref(){}
-    ref(T *ptr) : ptr(ptr), refCounter(new ReferenceCounter()) {}
-    ref(T ptr) : ptr(new T(ptr)), refCounter(new ReferenceCounter()) {}
+    ref() : ptr(nullptr), refCounter(new ReferenceCounter()) {}
+    ref(T *ptr = nullptr) : ptr(ptr), refCounter(new ReferenceCounter()) {}
+    ref(T &obj) : ptr(new T(obj)), refCounter(new ReferenceCounter()) {}
     ref(const ref<T> &sp) :
         ptr(sp.ptr), refCounter(sp.refCounter) {
         refCounter->addReference();
@@ -67,6 +67,7 @@ public:
     T &operator*() const {
         return *ptr;
     }
+
     void reset(T *ptr) {
         if (refCounter->removeReference() == 0) {
             delete this->ptr;
@@ -76,40 +77,43 @@ public:
         this->ptr = ptr;
         refCounter = new ReferenceCounter();
     }
+
     ref<T> operator+(const ref<T> &rhs) {
-        return (((this->op_add(new T(*ptr),new T(*rhs)))));
+        return this->op_add(*ptr, *rhs.ptr);
     }
     ref<T> operator-(const ref<T> &rhs) {
-        return (((this->op_sub(new T(*ptr),new T(*rhs)))));
+        return this->op_sub(*ptr, *rhs.ptr);
     }
     ref<T> operator*(const ref<T> &rhs) {
-        return (this->op_mul(new T(*ptr),new T(*rhs)));
+        return this->op_mul(*ptr, *rhs.ptr);
     }
     ref<T> operator/(const ref<T> &rhs) {
-        return (this->op_div(new T(*ptr),new T(*rhs)));
+        return this->op_div(*ptr, *rhs.ptr);
     }
     auto operator%(ref<T> op){
-      return this->op_mod(new T(*ptr),new T(*op));
+    return this->op_mod(*ptr, *op.ptr);
     }
     auto operator>(ref<T> op){
-      return this->op_greater(new T(*ptr),new T(*op));
+    return this->op_greater(*ptr, *op.ptr);
     }
     auto operator<(ref<T> op){
-      return this->op_lesser(new T(*ptr),new T(*op));
+    return this->op_lesser(*ptr, *op.ptr);
     }
     auto operator>=(ref<T> op){
-      return this->op_greaterEqual(new T(*ptr),new T(*op));
+    return this->op_greaterEqual(*ptr, *op.ptr);
     }
     auto operator<=(ref<T> op){
-      return this->op_lesserEqual(new T(*ptr),new T(*op));
+    return this->op_lesserEqual(*ptr, *op.ptr);
     }
     auto operator !=(ref<T> op){
-      return this->op_notEqual(new T(*ptr),new T(*op));
+    return this->op_notEqual(*ptr, *op.ptr);
     }
     auto operator ==(ref<T> op){
-      return this->op_equal(new T(*ptr),new T(*op));
+    return this->op_equal(*ptr, *op.ptr);
     }
+
     auto operator[](int index);
+
 };
 
 #endif // REFERENCECOUNT_H_CSQ4
