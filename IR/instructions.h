125ded38917a642e9153fbadf89e79e599e35a2b
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-03-05 18:51:45 +0530

    Create instructions.h

---
+++ IR/instructions.h
@@ -0,0 +1,14 @@
+#if !defined(instructions_IRCsq4)
+#define instructions_IRCsq4
+
+enum instructions{
+    PUSH,
+    DEF,
+    FOR,
+    WHILE,
+    CLASS,
+    IMPORT
+    
+};
+
+#endif // instructions_IRCsq4
