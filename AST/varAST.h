2f0c1cfa55cd32f2d098216e80640a06d1ab9a8c
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-03-06 21:54:04 +0530

    Update varAST.h

--- AST/varAST.h
+++ AST/varAST.h
@@ -3,10 +3,18 @@
 #include "../Tokenizer/tokenizer.h"
 #include "ast.h"
 #include "../Parser/variables.h"
-struct VarAST{
-    AST_TYPE type = VAR_ASSIGNMENT;
-    Variable variable;
+struct VarAST : AST{
+    AST_TYPE type = VAR_DECLARATION;
+    string name;
+    AST* value;
 };
+
+VarAST addVariable(string name, AST* value){
+    VarAST var;
+    var.name = name;
+    var.value = value;
+    return var;
+}
 
 
 #endif // varAST_Csq4
