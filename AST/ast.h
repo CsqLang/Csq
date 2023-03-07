75694b7c8603432ff8ecd037d8f0bd04273aed42
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-03-07 14:57:02 +0530

    Update ast.h

--- AST/ast.h
+++ AST/ast.h
@@ -2,7 +2,6 @@
 #define AST_Csq4_H
 #include "../Grammar/grammar.h"
 #include "../Tokenizer/tokenizer.h"
-
 //Types of AST
 enum AST_TYPE{
     VALUE_TYPE = 1,
@@ -12,11 +11,13 @@ enum AST_TYPE{
     CLASS_DEFINITION = 5,
     FOR_LOOP = 6,
     WHILE_LOOP = 7,
+    BINARY_EXP = 8,
 };
 
 //Base class for all AST.
 struct AST{
     AST_TYPE type;
 };
+
 
 #endif // AST_Csq4_H
