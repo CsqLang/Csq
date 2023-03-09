b3ca10988ed03c52193181a13a9739c2f2cd66b7
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-03-09 14:57:39 +0530

    Update instructions.h

--- IR/instructions.h
+++ IR/instructions.h
@@ -1,14 +1,30 @@
 #if !defined(instructions_IRCsq4)
 #define instructions_IRCsq4
+#include "../AST/ast.h"
+
 
 enum instructions{
-    PUSH,
+    PUSHVAR,
     DEF,
     FOR,
     WHILE,
+    IF,
+    ELIF,
+    ELSE,
     CLASS,
-    IMPORT
-    
+    PUSHVALUE,
 };
+
+
+/*
+    Example::
+    Csq code: a = 49
+    AST representation:
+    Variable Declaration
+        /       \
+       a        49
+    IR representation:
+    PUSHVAR a 49
+*/
 
 #endif // instructions_IRCsq4
