744bf85e07310afaac30ff90ab9c5cdbbd29dc72
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-03-11 12:44:10 +0530

    Update parser.h

--- Parser/parser.h
+++ Parser/parser.h
@@ -1,1 +1,15 @@
+#if !defined(PARSEr_H_CSQ4)
+#define PARSEr_H_CSQ4
+    #include "../Tokenizer/tokenizer.h"
+    #include "../AST/ast.h"
 
+    //Tools required for parsing
+    /*
+        Check tokens.
+    */
+
+    Node* Parse(vector<Token> tokens){
+
+    }
+
+#endif // PARSEr_H_CSQ4
