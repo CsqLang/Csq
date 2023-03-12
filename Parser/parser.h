48e0f96cb2a7b71b65765cb24c1ca9e50e7eb2ea
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-03-12 19:26:47 +0530

    Update parser.h

--- Parser/parser.h
+++ Parser/parser.h
@@ -56,10 +56,30 @@
         if(tokens[0].type == IDENTIFIER && tokens[1].type == ASOPERATOR && tokens[2].type != ASOPERATOR && in(tokens[0].token,Variables) == 1)
             state = true;
         return state;
+    }       
+    //This will be useful to do parsing.
+    int current_index = 0;
+    //This represents the stream of tokens.
+    vector<Token> Tokens;
+    //function to traverse the stream of tokens one by one.
+    Token next(){
+        current_index++;
+        return Tokens[current_index-1];
     }
 
+    //function to do parsing and return AST node.
     Node* ParseStatement(vector<Token> tokens){
         Node* node;
+        Tokens = tokens;
+        if(isVarDecl(tokens)){
+            VarDecl varNode;
+            varNode.type = VAR_DECLARATION;
+            varNode.name = tokens[0].token;
+            /*If this if block is implemented it means it has passed conditions of VarDecl so 
+            as tokens[1] there will be '=' so we should work upon values so to do so we could
+            do one more check to find the type of values like BinaryExpr or any else.
+            */
+        }
         return node;
     }
 
