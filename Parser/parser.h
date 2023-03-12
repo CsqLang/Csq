3d8aeae0eb4821470ba60ca8e1e83f1d6307b88b
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-03-12 16:57:18 +0530

    Update parser.h

--- Parser/parser.h
+++ Parser/parser.h
@@ -2,14 +2,65 @@
 #define PARSEr_H_CSQ4
     #include "../Tokenizer/tokenizer.h"
     #include "../AST/ast.h"
+    #include "../Memory/stack.h"
 
     //Tools required for parsing
+
+    /*Exceptions required for parsing and finding out the mistakes earlier.*/
+
     /*
         Check tokens.
     */
+    bool parenthesisPresent(vector<Token> tokens){
+        vector<string> strtokens;
+        bool presence = false;
+        for(Token token : tokens)
+            strtokens.push_back(token.token);
+        if(in("(", strtokens) || in(")", strtokens)){
+            presence = true;
+        }
+        return presence;
+    }
 
-    Node* Parse(vector<Token> tokens){
+    bool squareBracPresent(vector<Token> tokens){
+        vector<string> strtokens;
+        bool presence = false;
+        for(Token token : tokens)
+            strtokens.push_back(token.token);
+        if(in("[", strtokens) || in("]", strtokens)){
+            presence = true;
+        }
+        return presence;
+    }
 
+    bool curlyBracesPresent(vector<Token> tokens){
+        vector<string> strtokens;
+        bool presence = false;
+        for(Token token : tokens)
+            strtokens.push_back(token.token);
+        if(in("{", strtokens) || in("}", strtokens)){
+            presence = true;
+        }
+        return presence;
+    }
+
+    bool isVarDecl(vector<Token> tokens){
+        bool state = false;
+        if(tokens[0].type == IDENTIFIER && tokens[1].type == ASOPERATOR && tokens[2].type != ASOPERATOR && in(tokens[0].token,Variables) == 0)
+            state = true;
+        return state;
+    }
+
+    bool isVarAssign(vector<Token> tokens){
+        bool state = false;
+        if(tokens[0].type == IDENTIFIER && tokens[1].type == ASOPERATOR && tokens[2].type != ASOPERATOR && in(tokens[0].token,Variables) == 1)
+            state = true;
+        return state;
+    }
+
+    Node* ParseStatement(vector<Token> tokens){
+        Node* node;
+        return node;
     }
 
 #endif // PARSEr_H_CSQ4
