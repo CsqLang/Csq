4c250ba486ec23fddf7d75dbc59fbe1952dcfbe4
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-03-14 19:16:15 +0530

    Update ast.h

--- AST/ast.h
+++ AST/ast.h
@@ -24,94 +24,14 @@ typedef enum {
     ELSE_STATEMENT,
 } NODE_TYPE;
 
-typedef struct Node {
-    NODE_TYPE type;
-    union {
-        struct {
-            char* value;
-        } value_node;
-        struct {
-            char* name;
-            std::unique_ptr<Node> value;
-        } var_declaration_node;
-        struct {
-            char* name;
-            std::unique_ptr<Node> value;
-        } var_assignment_node;
-        struct {
-            char* name;
-            std::unique_ptr<Node> args;
-            std::unique_ptr<Node> body;
-        } fun_definition_node;
-        struct {
-            char* name;
-            std::unique_ptr<Node> body;
-        } class_definition_node;
-        struct {
-            std::unique_ptr<Node> initialization;
-            std::unique_ptr<Node> condition;
-            std::unique_ptr<Node> update;
-            std::unique_ptr<Node> body;
-        } for_loop_node;
-        struct {
-            std::unique_ptr<Node> condition;
-            std::unique_ptr<Node> body;
-        } while_loop_node;
-        struct {
-            char* op;
-            std::unique_ptr<Node> left;
-            std::unique_ptr<Node> right;
-        } expr_type_node;
-        struct {
-            char* name;
-            std::unique_ptr<Node> args;
-        } function_call_node;
-        struct {
-            char* name;
-            std::unique_ptr<Node> args;
-            std::unique_ptr<Node> body;
-        } function_decl_node;
-        struct {
-            std::unique_ptr<Node> statements;
-        } block_node;
-        struct {
-            std::unique_ptr<Node> condition;
-            std::unique_ptr<Node> body;
-        } if_statement_node;
-        struct {
-            std::unique_ptr<Node> condition;
-            std::unique_ptr<Node> body;
-        } elif_statement_node;
-        struct {
-            std::unique_ptr<Node> body;
-        } else_statement_node;
-    };
-} Node;
+//Node struct
+struct Node;
+//A short alias for unique_ptr<Node>
+typedef std::unique_ptr<Node> Nodetype;
 
-// Function to create a new value node
-std::unique_ptr<Node> create_value_node(char* value) {
-    auto node = std::make_unique<Node>();
-    node->type = VALUE_TYPE;
-    node->value_node.value = value;
-    return node;
-}
-
-// Function to create a new variable declaration node
-std::unique_ptr<Node> create_var_declaration_node(char* name, std::unique_ptr<Node> value) {
-    auto node = std::make_unique<Node>();
-    node->type = VAR_DECLARATION;
-    node->var_declaration_node.name = name;
-    node->var_declaration_node.value = std::move(value);
-    return node;
-}
-
-// Function to create a new variable assignment node
-std::unique_ptr<Node> create_var_assignment_node(char* name, std::unique_ptr<Node> value) {
-    auto node = std::make_unique<Node>();
-    node->type = VAR_ASSIGNMENT;
-    node->var_assignment_node.name = name;
-    node->var_assignment_node.value = std::move(value);
-    return node;
-}
+//body struct for node type
+struct Node{
+    
+};
 
 #endif // AST_Csq4_H
