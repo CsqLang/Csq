654e321e1132d22fe3e54dc35ff60bed7c6e5634
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-03-18 20:59:46 +0530

    Update tokenizer.h

--- Tokenizer/tokenizer.h
+++ Tokenizer/tokenizer.h
@@ -19,6 +19,35 @@ enum TokenType {
     UNKNOWN = 11,
 };
 
+//This enum field will give even more detail about symbols excluding identifiers, numbrs and keywords
+typedef enum{
+    LBRACE = 1,
+    RBRACE = 2,
+    LPAREN = 3,
+    RPAREN = 4,
+    LBRACK = 5,
+    RBRACK = 6,
+    PERCENT = 7,
+    EQUAL = 8,
+    TILDE = 9,
+    COMMA = 10,
+    GREATER = 11,
+    LESSER = 12,
+    SEMI = 13,
+    COLAN = 14,
+    PLUS = 15,
+    MINUS = 16,
+    STAR = 17,
+    BSLASH = 18,
+    HASHTAG = 19,
+    AMPER = 20,
+    VBAR = 21,
+    NOT = 22,
+    POWER = 23,
+    NOTSYMBOL = 24,
+    DOT = 25,
+}STOKEN;
+
 //Struct for tokens
 struct Token{
     string token;
@@ -179,6 +208,43 @@ Token check(string val,int line){
     return token;
 }
 
+//This function will return a brief info about the symbols.
+STOKEN symbolType(Token token){
+    switch(token.type){
+        case ASOPERATOR:{
+            if(token.token == "=")
+                return EQUAL;
+        }
+        case AROPERATOR:{
+
+        }
+        case SYMBOL:{
+            if(token.token == "!")
+                return NOT;
+            else if(token.token == "|")
+                return VBAR;
+            else if(token.token == "&")
+                return AMPER;
+            else if(token.token == "{")
+                return LBRACE;
+            else if(token.token == "}")
+                return RBRACE;
+            else if(token.token == "(")
+                return LPAREN;
+            else if(token.token == ")")
+                return RPAREN;
+            else if(token.token == "[")
+                return LBRACK;
+            else if(token.token == "]")
+                return RBRACK;
+        }
+        default:{
+            return NOTSYMBOL;
+        }
+    }
+}
+
+
 vector<Token> tokenize(string source_code, int line_no) {
     vector<Token> tokens;
     string current_string, str_input;
@@ -232,7 +298,35 @@ vector<Token> tokenize(string source_code, int line_no) {
         }
     }
 
-    return tokens;
+    vector<Token> filtered_Tokens;
+    for(int i = 0;i<tokens.size();i++){
+        switch(tokens[i].type){
+            case ASOPERATOR:{
+                switch(tokens[i+1].type)
+                    case ASOPERATOR:{
+                        Token token;
+                        token.type = COPERATOR;
+                        token.token = "==";
+                        filtered_Tokens.push_back(token);
+                        i = i + 2;
+                    }
+            }
+            case SYMBOL:{
+                switch(tokens[i+1].type)
+                    case ASOPERATOR:{
+                        Token token;
+                        token.type = COPERATOR;
+                        token.token = "!=";
+                        filtered_Tokens.push_back(token);
+                        i = i + 2;
+                    }
+            }
+            default:
+                filtered_Tokens.push_back(tokens[i]);
+        }
+    }
+
+    return filtered_Tokens;
 }
 
 #endif // tokenizer_Csq4
