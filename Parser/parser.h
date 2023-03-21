e0f72240638f337ba3f44378ad0ce5c35c6aa27a
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-03-21 11:53:35 +0530

    Update parser.h

--- Parser/parser.h
+++ Parser/parser.h
@@ -23,17 +23,56 @@
     //Root for all nodes.
     Program root;
 
+
     //Scope for the statements
     struct Scope{
         int indent_level;
         string id;
+        Scope(){}
         Scope(int level, string id_){
             indent_level = level;
             id = id_;
         }
     };
 
+    //Getting current indentation level.
+    int getIndentLevel(TokenStream tokens){
+        int indent_level = 0;
+        if(tokens[0].type != INDENT)
+            return indent_level;
+        else{
+            for(Token token : tokens){
+                if(token.type == INDENT)
+                    indent_level++;
+                else
+                    break; 
+            }
+            return indent_level;
+        }
+    }
+
+    vector<TokenStream> Tokenizer(string code){
+        code += "\n";
+        vector<TokenStream> lines;
+        string current = "";
+        for(char ch : code){
+            if(ch != '\n')
+                current.push_back(ch);
+            else{
+                lines.push_back(tokenize(current));
+                current = "";
+            }
+        }
+        return lines;
+    }
     
-    
+    IfStmt ParseIfStmt(TokenStream tokens);
+    ElifStmt ParseElifStmt(TokenStream tokens);
+    ElseStmt ParseElseStmt(TokenStream tokens);
+
+    // vector<Block,TokenStream>  ParseScope(TokenStream raw_tokens, int indent_level,string id = ""){
+    //     vector<TokenStream> tokens = LineSeperator(raw_tokens);
+    //     return tokens;
+    // }
 
 #endif // PARSEr_H_CSQ4
