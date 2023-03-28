7066485b918fbc79df745518b6e34f8ad5c13b69
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-03-28 17:37:56 +0530

    Create parser_test.cpp

---
+++ Parser/parser_test.cpp
@@ -0,0 +1,13 @@
+/*
+
+This is the test to check whether tokenizer is working perfectly or not?
+
+*/
+#include <iostream>
+#include "AST/ast.h"
+#include "Parser/parser.h"
+int main() {
+    auto tokens = Tokenizer("def hello():\n 2+8\n2+8\ndef df():\n 2=4\n33+3");
+    ParseLines(tokens);
+    cout << ParseStatements() << "\n";
+}
