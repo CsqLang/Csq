d24909800a3df6294bdbc8c5660bb708b78e3674
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-03-30 13:59:33 +0530

    Update parser_test.cpp

--- Parser/parser_test.cpp
+++ Parser/parser_test.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
-#include "../AST/ast.h"
+#include "AST/ast.h"
 #include "Parser/parser.h"
-int main() {
-    auto tokens = Tokenizer("def hello():\n 2+8\n2+8\ndef df():\n 2=4\n33+3");
+
+
+void testMultipleFunctions(){
+
+    string code = "def fun1():\n print('inside fun1')\ndef fun2():\n print('inside fun2')\n";
+
+    auto tokens = Tokenizer(code);
     ParseLines(tokens);
     cout << ParseStatements() << "\n";
+}
+
+void testIfElseLadder(){
+
+    string code = "if 1 == 2:\n print('Your math is wrong')\nelse:\n print('Your math is correct')";
+
+    auto tokens = Tokenizer(code);
+    ParseLines(tokens);
+    cout << ParseStatements() << "\n";
+}
+
+
+int main() {
+    testIfElseLadder();
 }
