024e8384f3d8173d751bd4ea3567f88e3db8eec5
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-04-06 19:57:03 +0530

    Create csq.cpp

---
+++ csq.cpp
@@ -0,0 +1,19 @@
+#include "AST/ast.h"
+#include "AST/visitor.h"
+#include "Parser/parser.h"
+#include "Tokenizer/tokenizer.h"
+#include "IR/instructions.h"
+#include "IR/vm.h"
+
+//The transpiler for Csq4.2
+int main(int argc, char const *argv[])
+{
+        if(argc < 4){
+            printf("Expected 3 args : <lang> <name> <current dir>\n");
+        }
+        else{
+            compile(argv[1],argv[3],argv[2]);
+            RunExecutable(string(argv[3]) + "/" + argv[2]);
+        }
+    return 0;
+}
