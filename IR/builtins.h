fce0853fa17d634d5c0d8b6e9c519f1753dc7ef1
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-04-07 11:33:00 +0530

    Update builtins.h

--- IR/builtins.h
+++ IR/builtins.h
@@ -1,6 +1,7 @@
 #if !defined(BUILTINS_CSQ4_2)
 #define BUILTINS_CSQ4_2
 #include "vm.h"
+
 struct i32{
     int data;
     string __str__;
@@ -10,6 +11,73 @@ struct i32{
     i32 operator+(const i32& val){
         return i32(data + val.data);
     }
+    i32 operator-(const i32& val){
+        return i32(data - val.data);
+    }
+    i32 operator/(const i32& val){
+        return i32(data / val.data);
+    }
+    i32 operator*(const i32& val){
+        return i32(data * val.data);
+    }
+    bool operator==(const i32& val){
+        return data == val.data;
+    }
+    bool operator!=(const i32& val){
+        return data != val.data;
+    }
+    bool operator>(const i32& val){
+        return data > val.data;
+    }
+    bool operator<(const i32& val){
+        return data < val.data;
+    }
+    bool operator>=(const i32& val){
+        return data >= val.data;
+    }
+    bool operator<=(const i32& val){
+        return data <= val.data;
+    }
 };
+
+struct i64 {
+    long long data;
+    string __str__;
+    i64() { data = 0; __str__ = to_string(data); }
+    i64(long long val) { data = val; __str__ = to_string(data); }
+    i64(const i64& val) { data = val.data; __str__ = to_string(data); }
+    i64 operator+(const i64& val) {
+        return i64(data + val.data);
+    }
+    i64 operator-(const i64& val) {
+        return i64(data + val.data);
+    }
+    i64 operator*(const i64& val) {
+        return i64(data + val.data);
+    }
+    i64 operator/(const i64& val) {
+        return i64(data / val.data);
+    }
+    bool operator==(const i64& val){
+        return data == val.data;
+    }
+    bool operator!=(const i64& val){
+        return data != val.data;
+    }
+    bool operator>(const i64& val){
+        return data > val.data;
+    }
+    bool operator<(const i64& val){
+        return data < val.data;
+    }
+    bool operator>=(const i64& val){
+        return data >= val.data;
+    }
+    bool operator<=(const i64& val){
+        return data <= val.data;
+    }
+};
+
+
 
 #endif // BUILTINS_CSQ4_2
