076d5e2566a39babd1a3d58365ef08635205f5ba
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-04-22 13:43:26 +0530

    Update ast.h

--- AST/ast.h
+++ AST/ast.h
@@ -1,160 +1,248 @@
-/*This is the implementation for visitor for AST.*/
-#if !defined(VISITOR_H_CSQ4)
-#define VISITOR_H_CSQ4
+#if !defined(AST_Csq4_H)
+#define AST_Csq4_H
 
-#include "ast.h"
+//Imports
+#include "../Grammar/grammar.h"
+#include "../Tokenizer/tokenizer.h"
+#include "../Memory/stack.h"
+#include <memory>
+#include <stack>
+using namespace std;
+
+//Alias for shared_ptr;
+#define Ptr shared_ptr
 
 
-string VarDecl_visitor(Ptr<VarDecl> node);
-string VarAssign_visitor(Ptr<VarAssign> node);
-string FuncDecl_visitor(Ptr<FunctionDecl> node);
-string IfStmt_visitor(Ptr<IfStmt> node);
-string ElifStmt_visitor(Ptr<ElifStmt> node);
-string ElseStmt_visitor(Ptr<ElseStmt> node);
-string ForLoop_visitor(Ptr<ForLoop> node);
-string WhileLoop_visitor(Ptr<WhileLoop> node);
-string ClassDecl_visitor(Ptr<ClassDecl> node);
-string Block_visitor(Ptr<Block> node);
-string Break_visitor(Ptr<Break> node);
-string Group_visitor(Ptr<Group> node);
+//Node types
+typedef enum {
+    VAR_DECLARATION,
+    VAR_ASSIGNMENT,
+    CLASS_DEFINITION,
+    FOR_LOOP,
+    WHILE_LOOP,
+    EXPR_TYPE,
+    FUNCTION_DECL,
+    BLOCK,
+    IF_STATEMENT,
+    ELIF_STATEMENT,
+    ELSE_STATEMENT,
+    FUN_ARGUMENT,
+    BREAK,
+    GROUP,
+    PROGRAM, //The basic node type is Program which doesn't needs to be traversed.
+} NODE_TYPE;
 
-//Visitor for variable declaration and assignment
-string VarDecl_visitor(Ptr<VarDecl> node){
-    if(node->type_infr){
-        return "VAR " + node->name + " = " + node->value.expr;
+//Node struct
+struct Node;
+//body struct for node type
+struct Node{
+    NODE_TYPE type;
+    Node(NODE_TYPE nodetype){
+        type = nodetype;
     }
-    else{
-        return node->type_ + " " + node->name + " = " + node->value.expr;
+    Node(){}
+};
+
+//Declaration for Node types.
+struct Expr;
+struct VarDecl;
+struct VarAssign;
+struct Block;
+struct FunctionDecl;
+struct ClassDecl;
+struct ForLoop;
+struct WhileLoop;
+struct IfStmt;
+struct ElifStmt;
+struct ElseStmt;
+struct FunArg;
+struct Break;
+struct Group;
+// Definitions for above Node types
+
+struct Expr : Node{
+    string expr;
+    Expr(){type = EXPR_TYPE; expr = "";}
+    Expr(string express){type = EXPR_TYPE; expr = express;}
+};
+
+struct VarDecl : Node{
+    string name;
+    Expr value;
+    bool type_infr;
+    string type_;
+    VarDecl(string name_, string value_, string type_ = ""){
+        name = name_;
+        value = value_;
+        type = VAR_DECLARATION;
+        if(type_ == ""){
+            type_infr = true;
+        }
+        else{
+            type_infr = false;
+        }
     }
-}
-string VarAssign_visitor(Ptr<VarAssign> node){
-    return node->name + " = " + node->value.expr;
-}
-string Break_visitor(Ptr<Break> node){
-    return "BREAK";
-}
-//Visitor for blocks
-string Block_visitor(Ptr<Block> node){
-    string code = "{\n";
-    for(string statement : node->statements)
-        code += statement + ";\n";
-    return code + "}\n";
-}
+    VarDecl(){
+        name = "";
+        value = Expr();
+        type = VAR_DECLARATION;
 
-//Visitor for If,Elif and Else statements
-string IfStmt_visitor(Ptr<IfStmt> node){
-    return "IF(" + node->condition.expr + ")\n";
-}
-string ElifStmt_visitor(Ptr<ElifStmt> node){
-    return "ELIF(" + node->condition.expr + ")\n";
-}
-string ElseStmt_visitor(Ptr<ElseStmt> node){
-    return "ELSE\n";
-}
-
-//Visitor for ForLoop and WhileLoop statements
-string ForLoop_visitor(Ptr<ForLoop> node){
-    return "FOR( VAR " + node->iter_name + " : " + node->condition.expr + ")\n";
-}
-string WhileLoop_visitor(Ptr<WhileLoop> node){
-    return "WHILE(" + node->condition.expr + ")\n";
-}
-
-//Visitor for Function declarations.
-string FuncDecl_visitor(Ptr<FunctionDecl> node){
-    string params,code;
-    if(node->params.size()>0 && node->params[0]!=""){
-        for(string param : node->params){
-            params += "auto " + param + ", ";
-        }
-        params.pop_back();
-        params.pop_back();
     }
-    shared_ptr<Block> block = make_shared<Block>();
-    for(string statement : node->body.statements)
-        addStatement(block,statement);
-    if(node->return_type == ""){
-        return "FUN " + node->name + "= [&] ( " + params + ")\n" + "//"+node->return_type + "\n";
+};
+
+struct Break : Node{
+    Break(){
+        type = BREAK;
     }
-    else{
-        return node->return_type + " " + node->name + "( " + params + ")\n" + "//"+node->return_type + "\n";
+};
+
+struct VarAssign : Node{
+    string name;
+    Expr value;
+    VarAssign(string name_, string value_){
+        name = name_;
+        value = value_;
+        type = VAR_ASSIGNMENT;
     }
-}
-
-string Expr_visitor(Ptr<Expr> node){
-    return node->expr;
-}
-
-string ClassDecl_visitor(Ptr<ClassDecl> node){
-    string code;
-    code += "CLASS " + node->name + " ";
-    if(node->inherit_class != ""){
-        code += " : " + node->inherit_class;
+    VarAssign(){
+        name = "";
+        value = Expr();
+        type = VAR_ASSIGNMENT;
     }
-    return code;
-}
+};
 
-string Group_visitor(Ptr<Group> node){
-    string code;
-    code += "GROUP " + node->name + "\n";
-    return code;
-}
+struct Block : Node{
+    vector<string> statements;
+    Block(){type = BLOCK;}
+    Block(vector<string> statement){statements = statement;type = BLOCK;}
+};
 
-//Definition for visit function 
-string visit(const Ptr<Node>& node) {
-    switch (node->type) {
-        case VAR_DECLARATION: {
-            Ptr<VarDecl> var = static_pointer_cast<VarDecl>(node);
-            return VarDecl_visitor(var);
-        }
-        case VAR_ASSIGNMENT: {
-            Ptr<VarAssign> var = static_pointer_cast<VarAssign>(node);
-            return VarAssign_visitor(var);
-        }
-        case BLOCK:{
-            Ptr<Block> block = static_pointer_cast<Block>(node);
-            return Block_visitor(block);
-        }
-        case FUNCTION_DECL:{
-            Ptr<FunctionDecl> fun = static_pointer_cast<FunctionDecl>(node);
-            return FuncDecl_visitor(fun);
-        }
-        case FOR_LOOP:{
-            Ptr<ForLoop> floop = static_pointer_cast<ForLoop>(node);
-            return ForLoop_visitor(floop);
-        }
-        case WHILE_LOOP:{
-            Ptr<WhileLoop> stmt = static_pointer_cast<WhileLoop>(node);
-            return WhileLoop_visitor(stmt);
-        }
-        case IF_STATEMENT:{
-            Ptr<IfStmt> stmt = static_pointer_cast<IfStmt>(node);
-            return IfStmt_visitor(stmt);
-        }
-        case ELIF_STATEMENT:{
-            Ptr<ElifStmt> stmt = static_pointer_cast<ElifStmt>(node);
-            return ElifStmt_visitor(stmt);
-        }
-        case ELSE_STATEMENT:{
-            Ptr<ElseStmt> stmt = static_pointer_cast<ElseStmt>(node);
-            return ElseStmt_visitor(stmt);
-        }
-        case BREAK:{
-            Ptr<Break> stmt = static_pointer_cast<Break>(node);
-            return Break_visitor(stmt);
-        }
-        case CLASS_DEFINITION:{
-            Ptr<ClassDecl> stmt = static_pointer_cast<ClassDecl>(node);
-            return ClassDecl_visitor(stmt);
-        }
-        case GROUP:{
-            Ptr<Group> stmt = static_pointer_cast<Group>(node);
-            return Group_visitor(stmt);
-        }
-        default:
-            Ptr<Expr> stmt = static_pointer_cast<Expr>(node);
-            return Expr_visitor(stmt);
+struct FunctionDecl : Node{
+    string name;
+    string return_type;
+    bool return_type_infr;
+    vector<string> params = {};
+    Block body;
+    FunctionDecl(){
+        type = FUNCTION_DECL;
+        name = "";
     }
+    FunctionDecl(string name_, vector<string> params_, string return_type_=""){
+        type = FUNCTION_DECL;
+        name = name_;
+        params = params_;
+        if(return_type_ == ""){
+            return_type = "";
+            return_type_infr = 1;
+        }
+        else{
+            return_type = return_type_;
+            return_type_infr = 0;
+        }
+    }
+};
+
+struct ForLoop : Node{
+    Expr condition;
+    string iter_name;
+    Block body;
+    ForLoop(){type = FOR_LOOP;}
+    ForLoop(string itername, Expr cond){type = FOR_LOOP;iter_name = itername;condition = cond;}
+};
+
+struct WhileLoop : Node{
+    Expr condition;
+    Block body;
+    WhileLoop(){type = WHILE_LOOP;}
+    WhileLoop(Expr cond){type = WHILE_LOOP;condition = cond;}
+};
+
+struct IfStmt : Node{
+    Expr condition;
+    Block body;
+    IfStmt(){type = IF_STATEMENT;}
+    IfStmt(Expr cond){type = IF_STATEMENT;condition = cond;}
+};
+
+struct ElifStmt : Node{
+    Expr condition;
+    Block body;
+    ElifStmt(){type = ELIF_STATEMENT;}
+    ElifStmt(Expr cond){type = ELIF_STATEMENT;condition = cond;}
+};
+
+struct ElseStmt : Node{
+    Block body;
+    ElseStmt(){type = ELSE_STATEMENT;}
+};
+
+struct ClassDecl : Node{
+    string name;
+    string inherit_class;
+    ClassDecl(){
+        type = CLASS_DEFINITION;
+    }
+    ClassDecl(string name_, string inherit_class_){
+        name = name_;
+        inherit_class = inherit_class_;
+        type = CLASS_DEFINITION;
+    }
+};
+
+struct Group : Node{
+    string name;
+    Group(){type = GROUP;}
+    Group(string name_){
+        name = name_;
+        type = GROUP;
+    }
+};
+
+//Functions to add nodes for ease of use.
+
+Ptr<Node> addNode(VarDecl decl){
+    auto node = make_shared<VarDecl>();
+    node->name = decl.name;
+    node->value = decl.value;
+    return static_pointer_cast<Node>(node);
 }
 
-#endif // VISITOR_H_CSQ4
+Ptr<Node> addNode(VarAssign decl){
+    auto node = make_shared<VarAssign>();
+    node->name = decl.name;
+    node->value = decl.value;
+    return static_pointer_cast<Node>(node);
+}
+
+Ptr<Node> addNode(Expr decl){
+    auto node = make_shared<Expr>();
+    node->expr = decl.expr;
+    return static_pointer_cast<Node>(node);
+}
+
+Ptr<Node> addNode(Block decl){
+    auto node = make_shared<Block>();
+    node->statements = decl.statements;
+    return static_pointer_cast<Node>(node);
+}
+
+Ptr<Node> addNode(FunctionDecl decl){
+    auto node = make_shared<FunctionDecl>();
+    node->body = decl.body;
+    node->name = decl.name;
+    node->params = decl.params;
+    return static_pointer_cast<Node>(node);
+}
+
+void addStatement(Ptr<Block> block, string statement){
+    block->statements.push_back(statement);
+}
+void addParam(Ptr<FunctionDecl> fun, string param){
+    fun->params.push_back(param);
+}
+
+
+//Importing visitor containing all utilities to visit AST.
+#include "visitor.h"
+
+#endif // AST_Csq4_H
