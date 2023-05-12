bed2a077c6f690043434ccea60afab021c90c587
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-05-12 19:41:48 +0530

    Update parser.h

--- Parser/parser.h
+++ Parser/parser.h
@@ -3,7 +3,24 @@
 
 #include "symbol_table.h"
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//Scope for the statements
+struct Scope{
+    int indent_level;
+    bool ended;
+    NODE_TYPE of;
+    Scope(){}
+    Scope(int level, NODE_TYPE of_, bool ended_){
+        indent_level = level;
+        of = of_;
+        ended = ended_;
+    };
+};
+ //Returns the last scope present in the given scope vector.
+Scope last_scope(vector<Scope> scope){
+    return scope[scope.size()-1];
+}
+
 //Tokenizer for each line seperation
 vector<TokenStream> Tokenizer(string code){
     code += "\n";
@@ -178,6 +195,58 @@ VarAssign ParseVarAssign(TokenStream tokens, int line){
     return node;
 }
 
+Expr ParseCondition(TokenStream tokens, int line){
+    Expr node;
+    for(Token token : tokens){
+        if(token.type == IDENTIFIER){
+            //Check is it defined?
+            if(table.isIdentifierDefined(token.token))
+            {
+                node.expr += token.token + " ";
+            }
+            else{
+                error(line,"undefined identifier '" + token.token + "' used.");
+            }
+        }
+        else{
+            if(token.token != ":=")
+            {
+                node.expr += token.token + " ";
+            }
+            else{
+                error(line,"invalid assignment operator := in condition.");
+            }
+        }
+    }
+    return node;
+}
+
+IfStmt ParseIfStmt(TokenStream tokens, int line){
+    IfStmt node;
+    TokenStream condition_expr;
+    bool condition = 0;
+    bool end = 0;
+    //
+    for(Token token : tokens){
+        if(!condition && !end && token.token == "if"){
+                condition = 1;
+        }
+        else if(condition && token.token != ":"){
+            condition_expr.push_back(token);
+        }
+        else if(condition && token.token == ":"){
+            end = 1;
+            condition = 0;
+            node.condition = ParseCondition(condition_expr,line);
+        }
+        else if(end){
+            error(line,"invalid token after the end of if stmt '" + token.token + "'.");
+        }
+    }
+    //
+    return node;
+}
+
 //These functions will be checking which type of the statement is and returns it's nodetype.
 bool isVarDecl(TokenStream tokens){
     bool state = 0;
@@ -195,6 +264,29 @@ bool isVarAssign(TokenStream tokens){
     return state;
 }
 
+bool isIfStmt(TokenStream tokens){
+    bool state = 0;
+    if(tokens[0].token == "if"){
+        state = 1;
+    }
+    return state;
+}
+
+bool isElifStmt(TokenStream tokens){
+    bool state = 0;
+    if(tokens[0].token == "elif"){
+        state = 1;
+    }
+    return state;
+}
+bool isElseStmt(TokenStream tokens){
+    bool state = 0;
+    if(tokens[0].token == "else"){
+        state = 1;
+    }
+    return state;
+}
+
 NODE_TYPE StatementType(TokenStream tokens){
     NODE_TYPE type;
     
@@ -204,10 +296,21 @@ NODE_TYPE StatementType(TokenStream tokens){
     else if(isVarAssign(tokens)){
         type = VAR_ASSIGNMENT;
     }
+    else if(isIfStmt(tokens)){
+        type = IF_STATEMENT;
+    }
     else{
         type = EXPR_TYPE;
     }
     return type;
+}
+bool isBlockStatement(NODE_TYPE type){
+    bool state = 0;
+    if(
+        type == IF_STATEMENT || type == ELSE_STATEMENT || type == ELIF_STATEMENT ||
+        type == FUNCTION_DECL || type == CLASS_DEFINITION || type == FOR_LOOP || type == WHILE_LOOP
+    ){state = 1;}
+    return state;
 }
 int getIndentLevel(TokenStream tokens){
     int indent = 0;
@@ -238,23 +341,51 @@ TokenStream removeIndent(TokenStream tokens){
     return newTokens;
 }
 
+
+//Parser
 string Parse(vector<TokenStream> code)
 {
     //Load the C written builtins
     load_builtins();
+
+    //Add a ignore statement at last so that indentations can work correctly
+    code.push_back(TokenStream({tokenize(string("ignore") + ";ignore;")}));
+
     //Resulting code
     string codeString = "";
-    //Scope managers
-    int scope = 0;
+
+    //Scope properties
     int line_no = 1;
-    int prev_scope = -1;
-    bool is_last_line_if_statement = false;
-    //////////////////////////////
+    Scope scope(0,PROGRAM,0);
+    vector<Scope> scope_stack = {scope};
+
+
+    //States
+    bool class_ = 0;
+    int last_indent = 0;    
+    //Last statement type
+    NODE_TYPE last_statement = PROGRAM;
     for(TokenStream line : code)
     {
         //Get the current scope by finding indents
         int indent_level = getIndentLevel(line);
-        
+     
+        //Work with the indentation levels
+        while(indent_level != last_scope(scope_stack).indent_level){
+            if(last_scope(scope_stack).of == FUNCTION_DECL || last_scope(scope_stack).of == CLASS_DEFINITION){
+                codeString += "};\n";
+                scope_stack.pop_back(); 
+                if(last_scope(scope_stack).of == CLASS_DEFINITION){
+                    class_ = 0;
+                }
+            }
+            else{
+                codeString += "}\n";
+                //Pop the previous scope since it's now closed.
+                scope_stack.pop_back(); 
+            }
+        }
+
         //Removing all indentation from the stream
         line = removeIndent(line);
 
@@ -274,11 +405,19 @@ string Parse(vector<TokenStream> code)
                 codeString += VarAssign_visitor(node) + ";\n";
                 break;
             }
+            case IF_STATEMENT:{
+                scope_stack.push_back(Scope(indent_level+1, StatementType(line), 0));
+                IfStmt node = ParseIfStmt(line,line_no);
+                codeString += IfStmt_visitor(node) + "{\n";
+                break;
+            }
             case EXPR_TYPE:{
                 Expr node = ParseExpr(line,line_no);
                 codeString += Expr_visitor(node) + ";\n";
                 break;
             }
+            last_statement = StatementType(line);
+            last_indent = indent_level;
         }
     }
     return codeString;
