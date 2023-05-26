bced9312bacbed7c42fad733689e1429746eb280
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-05-26 14:55:09 +0530

    Update core.h

--- Runtime/core.h
+++ Runtime/core.h
@@ -5,16 +5,17 @@ Csq's runtime is responsible for the memory management and other runtime feature
 #if !defined(RUNTIME_CORE_CSQ)
 #define RUNTIME_CORE_CSQ
 
-#include "object.h"
-#include "memory.h"
-#include "error.h"
-#include "function.h"
+
 /*
 This will be used by parser to resemble current line.
 */
 int line = 1;
 
-
+#include "error.h" 
+#include "object.h"
+#include "memory.h"
+#include "function.h"
+#include "jit.h"
 /*
 Possible types of symbol for the symbol table.
 */
@@ -38,6 +39,16 @@ We could even utilize this to do type checking easily.
 */
 map<string, Symbol> SymTable;
 
+int inTable(string name){
+    int s = 0;
+    for(pair<string, Symbol> p : SymTable){
+        if(p.first == name){
+            s = 1;
+            break;
+        }
+    }
+    return s;
+}
 
 /*
 Function to allocate variables 
@@ -47,7 +58,60 @@ object to add it to the symbol table.
 void allocateVar(string name, string type, TokenStream tokens)
 {
     Variable var;
-    
+    var.name = name;
+    var.type = type;
+    TokenStream value_;
+    for(Token token : tokens){
+        if(token.type == IDENTIFIER){
+            if(inTable(token.token))
+            {
+                //Get the Symbol
+                Symbol symbol = SymTable[token.token];
+                switch(symbol.type){
+                    case VARIABLE:{
+                        //Get the value present 
+                        string val = readCell(SymTable[token.token].var.value_address);
+                        if(isInt(val) || isDecimal(val)){
+                            Token tok_;
+                            tok_.token = val;
+                            tok_.type = VALUE;
+                            value_.push_back(tok_);
+                        }
+                        else{
+                            Token tok_;
+                            tok_.token = val;
+                            tok_.type = STR;
+                            value_.push_back(tok_);
+                        }
+                    }
+                }
+            }
+            else{
+                RuntimeError("undefined identifier '" + token.token + "'");
+            }
+        }
+        else{
+            value_.push_back(token);
+        }
+    }
+    //Allocate the data to the memory
+    addCell(eval(value_));
+    var.value_address = TopCellAddress();
+    Symbol sym_;
+    sym_.var = var;
+    sym_.type = VARIABLE;
+    SymTable[var.name] = sym_;
+}
+
+void traverseSymTable(){
+    for(pair<string,Symbol> p : SymTable){
+        if(p.second.type == VARIABLE){
+            printf("%s : { %d {%s %s %d} }\n",p.first.c_str(),p.second.type, p.second.var.name.c_str(), p.second.var.type.c_str(), p.second.var.value_address);
+        }
+        else{
+            // printf("%s : { %d {%s %s %d} }\n",p.first,p.second.type, p.second.var.name, p.second.var.type, p.second.var.value_address);
+        }
+    }
 }
 
 #endif // RUNTIME_CORE_CSQ
