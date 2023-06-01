c5895ba7d8976d9df9a6ec76383bef401959562b
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-06-01 12:39:27 +0530

    Update parser.h

--- Parser/parser.h
+++ Parser/parser.h
@@ -2,44 +2,39 @@
 #define PARSER_H_CSQ4
 #include "../Runtime/core.h"
 #include "../AST/ast.h"
+#include "../AST/visitor.h"
 
+/*
+Upper layer syntax checking in which we don't have to do much instead it will roughly match the 
+syntax and returns the possible type which could be futher verified.
+*/
 bool VarDecl_check(TokenStream tokens){
-    /*Syntax : <identifier> | : <type> = <value>
-                            | = <value>
+    /*Syntax : <identifier> | :=<value>
     
     */
     bool valid_state = false;
-    if(tokens[0].type == IDENTIFIER){
-        if(tokens[1].token == ":"){
-            valid_state = 1;
-            if(tokens[2].type == IDENTIFIER){
-                if(tokens[3].token == "="){
-                    if(tokens.size() > 4){}
-                    else{
-                        RuntimeError("expected a value after =");
+    if(tokens[0].type == IDENTIFIER)
+    {
+        if(tokens[1].token == ":=")
+        {
+            if(tokens.size()>2)
+            {
+                valid_state = 1;
+                for(int i = 2;i<tokens.size();i++){
+                    if(tokens[i].type == KEYWORD){
+                        RuntimeError("syntax error by using keyword '" + tokens[i].token + "' inside a value.");
+                        valid_state = 0;
+                    }
+                    else if(tokens[i].token == "="){
+                        RuntimeError("syntax error by using '" + tokens[i].token + "' inside a value.");
                         valid_state = 0;
                     }
                 }
-                else{
-                    RuntimeError("expected a '=' after type");
-                    valid_state = 0;
-                }   
             }
             else{
-                RuntimeError("expected an identifier after ':'.");
+                RuntimeError("expected a value after ':='.");
                 valid_state = 0;
             }
-        }
-        else if(tokens[1].token == "="){
-            if(tokens.size() > 2){valid_state = 1;}
-            else{
-                RuntimeError("expected a value after =");
-                valid_state = 0;
-            }
-        }
-        else{
-            RuntimeError("invalid identifier.");
-            valid_state = 0;
         }
     }
     return valid_state;
@@ -48,29 +43,56 @@ bool VarDecl_check(TokenStream tokens){
 //For variable reassignment
 bool VarAssign_Check(TokenStream tokens){
     bool valid = 0;
-    if(tokens[0].type == IDENTIFIER){
-        if(!inTable(tokens[0].token)){
-            if(tokens[1].token == "="){
-                if(tokens.size() > 2){
-                    valid = 1;
-                }
-                else{
-                    RuntimeError("expected a value after =");
-                    valid = 0;
-                }
+    if(tokens[0].type == IDENTIFIER)
+    {
+        if(tokens[1].token == "="){
+            if(tokens.size()>2){
+                valid = 1;
             }
             else{
-                RuntimeError("invalid token '" + tokens[1].token + "' after an identifier.");
+                RuntimeError("expected a value after '='.");
                 valid = 0;
             }
-        }
-        else{
-            RuntimeError("identifier '" + tokens[0].token +"'' already defined.");
         }
     }
     return valid;
 }
 
+//Function to parse expressions and report the errors.
+ExprNode parse_ExprNode(TokenStream tokens){
+    ExprNode node;
+
+    for(int i = 0;i<tokens.size();i++)
+    {   
+        Token token = tokens[i];
+        if(token.type == IDENTIFIER)
+        {
+            //Check is the identifier defined in table?
+            if(inTable(token.token))
+            {
+                Token val;
+                if(memory[SymTable[token.token].var.value_address].type == FLOAT){
+                    val.token = to_string(memory[SymTable[token.token].var.value_address].fval);
+                    val.type = VALUE;
+                }
+                else{
+                    val.token = memory[SymTable[token.token].var.value_address].sval;
+                    val.type = STR;
+                }
+                node.tokens.push_back(val);
+            }
+            else
+            {
+                RuntimeError("undefined name '" + token.token + "'.");
+            }
+        }
+        else{
+            //Just for now couldn't be used during actual production.
+            node.tokens.push_back(token);
+        }
+    }
+    return node;
+}
 
 //Parsing without reporting any error because when this function is called before it
 //VarDecl_check will be called.
@@ -78,10 +100,11 @@ VarDeclNode parse_VarDecl(TokenStream tokens){
     VarDeclNode node;
     //Default type:
     node.var_type = "any";
+    //lower level rep for value (in terms of tokens)
+    TokenStream valstream;
     //Parsing States
     bool name = 1;
     bool equal = 0;
-    bool type = 0;
     bool value = 0;
 
     //
@@ -91,26 +114,15 @@ VarDeclNode parse_VarDecl(TokenStream tokens){
             node.identifier = token.token;
             name = 0;
         }
-        else if(!name && !type && !equal && !value){
-            if(token.token == ":"){
-                type = 1;
-            }
-            else if(token.token == "="){
-                value = 1;
-                equal = 1;
-            }
+        else if(!equal && token.token == ":="){
+            value = 1;
+            equal = 1;
         }
-        else if(type){
-            if(token.token != "="){
-                node.var_type += token.token;
-            }else{
-                type = 0;
-            }
-        }
-        else if(value){
+        else if(equal){
             node.value.tokens.push_back(token);
         }
     }
+    
     return node;
 }
 //for var reassignment
@@ -135,6 +147,7 @@ VarAssignNode parse_VarAssign(TokenStream tokens){
     }
     return node;
 }
+
 
 /*
 "Now for Csq, now for the coding and the CsqLang"
