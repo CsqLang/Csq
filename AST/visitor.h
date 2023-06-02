2955d3b40b719b9d22edfe2eeb0f1bfb043dfed6
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-06-02 21:32:11 +0530

    Update visitor.h

--- AST/visitor.h
+++ AST/visitor.h
@@ -4,6 +4,9 @@
 
 #include "../Runtime/core.h"
 
+//
+string visit(ASTNode* node);
+
 string visit_ExprNode(ExprNode node){
     string result;
     for(Token token : node.tokens){
@@ -23,25 +26,52 @@ Cell visit_Call(CallNode node){
 
 string visit_VarDecl(VarDeclNode node){
     //This will also include the processing of the runtime variables
-    allocateVar(
-        node.identifier,
-        node.var_type,
-        node.value.tokens
-    );
+    // allocateVar(node.identifier,node.var_type,node.value.tokens);
+    //Embedding the function to string to convert it into C/C++ format so that they could execute it.
+    string code = "allocateVar(\"" + node.identifier + "\",\"" + node.var_type  + "\",\""+ tokenS_to_string(node.value.tokens) +"\");";
     //Basically we have returned "" because there is no need to generate any C/C++ var instead that is added to the virtual memory
     // thanks to allocateVar function.
-    return "";
+    return code;
 }
 
 string visit_VarAssign(VarAssignNode node){
     //It's processing is also not lengthy since we already implemented functions in the runtime core
-    assignVar(
-        node.identifier,
-        node.value.tokens
-    );
-    return "";
+    // assignVar(
+    //     node.identifier,
+    //     node.value.tokens
+    // );
+    string code = "assignVar(\"" + node.identifier + "\",\"" + tokenS_to_string(node.value.tokens) +"\");";
+    return code;
 }
 
+string visit_PrintNode(PrintNode node){
+    string code = "print(" + visit_ExprNode(node.value) + ");";
+    return code;
+}
 
+string visit_IfNode(IfStmtNode node){
+    string code = "if(" + visit_ExprNode(node.condition) + "){\n";
+    for(ASTNode* stmt : node.body.statements){
+        code += visit(stmt) + "\n";
+    }
+    return code;
+}
+
+string visit(ASTNode* node){
+    switch(node->type){
+        case VAR_DECL:{
+            VarDeclNode decl = *((VarDeclNode*)node);
+            return visit_VarDecl(decl);
+        }
+        case VAR_ASSIGN:{
+            VarAssignNode decl = *((VarAssignNode*)node);
+            return visit_VarAssign(decl);
+        }
+        case PRINT:{
+            PrintNode prt = *((PrintNode*)node);
+            return visit_PrintNode(prt);
+        }
+    }return "";
+}
 
 #endif // VISITOR_H_CSQ4
