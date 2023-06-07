609985f147cc5a7139328e2c03691136336ec629
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-06-07 14:59:22 +0530

    Create basic.h

---
+++ Builtin/basic.h
@@ -0,0 +1,10 @@
+#if !defined(basic_H)
+#define basic_H
+#include "../Runtime/memory.h"
+#include "../Runtime/core.h"
+
+void print(string arg){
+    printf("%s\n", arg.c_str());
+}
+
+#endif // basic_H
