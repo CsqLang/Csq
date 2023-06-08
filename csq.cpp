2e2024950159634fc9859aae2bd32c8b882ce10f
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-06-08 16:05:44 +0530

    Update csq.cpp

--- csq.cpp
+++ csq.cpp
@@ -2,22 +2,36 @@
 #include "AST/visitor.h"
 #include "Parser/parser.h"
 #include "Tokenizer/tokenizer.h"
-#include "IR/instructions.h"
-#include "IR/vm.h"
+#include "Runtime/memory.h"
+#include "Runtime/core.h"
+#include "wrapper.h"
+#include "Runtime/code_format.h"
 
-//The transpiler for Csq4.2
+
+
 int main(int argc, char const *argv[])
 {
         if(argc < 4){
             printf("Expected 3 args : <lang> <name> <current dir>\n");
         }
         else{
-            Pcurrent_dir = argv[3];
-            compile(argv[1],argv[3],argv[2]);
-            if(error_count == 0){
-                
-                RunExecutable(string(argv[3]) + "/" + argv[2]);
-            }
+            string lang = argv[1];
+            string name = argv[2];
+            string currdir = argv[3];
+
+            //Path to the file:
+            string path_code = currdir + "/" + name + ".csq";
+            //Read the file
+            string raw_code = readCode(path_code);
+            //Convert to tokens
+            vector<TokenStream> code = toTokens(raw_code);
+            //Now parse:
+            string _code = Parser(code);
+            _code = addBuiltin(currdir + "/") + "\n" + "int main(){\n" + _code + "\n}\n";
+            ofstream fobj(currdir + "/" + name + ".cpp");
+            fobj << _code << "\n";
+            //Execute the C++ code
+            system(("cd " + currdir + "/" + " && g++ " + name + ".cpp -o " + name + " && ./" + name).c_str());
         }
     return 0;
 }
