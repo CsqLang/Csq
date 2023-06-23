21db9f9d56ce86a1afa113a63c6e54d712f60904
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-06-23 21:19:47 +0530

    Update basic.h

--- Builtin/basic.h
+++ Builtin/basic.h
@@ -3,8 +3,12 @@
 #include "../Runtime/memory.h"
 #include "../Runtime/core.h"
 
-void print(string arg){
-    printf("%s\n", arg.c_str());
+void print(Cell arg){
+    if(arg.type == STRING){
+        printf("%s\n",arg.sval.c_str());
+    }
+    else{
+        printf("%lf\n",arg.fval);
+    }
 }
-
 #endif // basic_H
