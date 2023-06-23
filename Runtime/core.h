d4a4a3dddb8472a858ec4036b92c738bffc6157c
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-06-23 21:16:56 +0530

    Update core.h

--- Runtime/core.h
+++ Runtime/core.h
@@ -14,7 +14,7 @@ int line = 1;
 #include "object.h"
 #include "memory.h"
 #include "function.h"
-#include "jit.h"
+#include "eval.h"
 /*
 Possible types of symbol for the symbol table.
 */
@@ -49,117 +49,49 @@ int inTable(string name){
     return s;
 }
 
-/*
-Function to allocate variables 
-basically it first allocates the value into memory and then it creates Variable
-object to add it to the symbol table.
-*/
-void allocateVar(string name, string type, string val__)
-{
-    Variable var;
-    var.name = name;
-    var.type = type;
-    TokenStream value = tokenize(val__);
-    TokenStream nval;
-    //Filter identifiers by placing their values
-    for(int i = 0;i<value.size();i++){
-        Token token = value[i];
-        if(token.type == IDENTIFIER){
-            if(inTable(token.token)){
-                if(SymTable[token.token].type == VARIABLE){
-                    Cell cell = memory[(SymTable[token.token].var.value_address)];
-                    if(cell.type == STRING){
-                        nval.push_back(create(STR,cell.sval));
-                    }
-                    else if(cell.type == FLOAT){
-                        nval.push_back(create(VALUE,to_string(cell.fval)));
-                    }
-                    else{
-                        nval.push_back(create(VALUE,to_string(cell.ival)));
-                    }
-                }
-                else{
-                    // ... For functions
-                }
-            }
-            else{
-                RuntimeError("undefined identifier '" + token.token + "'.");
-            }
-        }
-        else{
-            nval.push_back(token);
-        }
-    }
-
-    var.value_address = eval(nval);
-    memory[var.value_address].u_count++;
-    Symbol sym;
-    sym.type = VARIABLE;
-    sym.var = var;
-    SymTable[name] = sym;
-}
-
-void assignVar(string name, string val_){
-    TokenStream nval;
-    TokenStream value = tokenize(val_);
-    //Filter identifiers by placing their values
-    for(int i = 0;i<value.size();i++){
-        Token token = value[i];
-        if(token.type == IDENTIFIER){
-            if(inTable(token.token)){
-                if(SymTable[token.token].type == VARIABLE){
-                    Cell cell = memory[(SymTable[token.token].var.value_address)];
-                    if(cell.type == STRING){
-                        nval.push_back(create(STR,cell.sval));
-                    }
-                    else if(cell.type == FLOAT){
-                        nval.push_back(create(VALUE,to_string(cell.fval)));
-                    }
-                    else{
-                        nval.push_back(create(VALUE,to_string(cell.ival)));
-                    }
-                }
-                else{
-                    // ... For functions
-                }
-            }
-            else{
-                RuntimeError("undefined identifier '" + token.token + "'.");
-            }
-        }
-        else{
-            nval.push_back(token);
-        }
-    }
-    if(memory[SymTable[name].var.value_address].u_count == 1){
-        int addr = SymTable[name].var.value_address;
-        memory.erase(memory.begin()+addr);
-        for(int mem_address = addr;mem_address < memory.size();mem_address++){
-            for(pair<string,Symbol> sym : SymTable){
-                if(SymTable[sym.first].var.value_address){
-                    SymTable[sym.first].var.value_address--;
-                }
-            }
-        }
-    }
-    else{
-        memory[SymTable[name].var.value_address].u_count--;
-    }
-    SymTable[name].var.value_address = eval(nval);
-    memory[SymTable[name].var.value_address].u_count++;
-}
-
-
 void traverseSymTable(){
     for(pair<string,Symbol> p : SymTable){
         if(p.second.type == VARIABLE){
             printf("%s : { %d Name: %s Type: %s Value : %d }\n",p.first.c_str(),p.second.type, p.second.var.name.c_str(), p.second.var.type.c_str(), p.second.var.value_address);
         }
-        else{
-            // printf("%s : { %d {%s %s %d} }\n",p.first,p.second.type, p.second.var.name, p.second.var.type, p.second.var.value_address);
-        }
+        else{}
     }
 }
+
+
+/*
+Logics****************
+Input : a == 2
+Output expected : 
+id('a')....
+*/
+
+Cell id(string identifier){
+    if(inTable(identifier)){
+        return memory[SymTable[identifier].var.value_address];
+    }
+    else{
+        printf("Error: undefined identifier '%s'.\n", identifier.c_str());
+        exit(0);
+        return Cell();
+    }
+}
+
+void allocateVar(string id_, string type, Cell c){
+    memory.push_back(c);
+    Symbol sym;
+    sym.var.name = id_;
+    sym.var.type = type;
+    sym.type = VARIABLE;
+    sym.var.value_address = TopCellAddress();
+    SymTable[id_] = sym;
+}
+
+void assignVar(string id_, Cell c){
+    memory.push_back(c);
+    SymTable[id_].var.value_address = TopCellAddress();
+}
+
 
 
 #endif // RUNTIME_CORE_CSQ
