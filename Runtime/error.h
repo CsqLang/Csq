686219c9a9fcb14113c4c97362f03167baa8a4bd
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-07-31 11:13:52 +0530

    Update error.h

--- Runtime/error.h
+++ Runtime/error.h
@@ -1,9 +1,8 @@
 #if !defined(ERROR_CSQ4)
 #define ERROR_CSQ4
 
-#include "memory.h"
-#include "core.h"
 
+#include "core.h"
 //Error counts
 int error_c = 0;
 /*
@@ -11,7 +10,7 @@ This file contains all types of error which can be returned during the execution
 */
 void RuntimeError(string error)
 {
-    printf("At line %d : %s\n", line,error.c_str());
+    printf("At line %d : %s\n", line_,error.c_str());
     error_c++;
 }
 
