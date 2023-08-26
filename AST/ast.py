05cd3af30ff414fc5022b32358814dd3e4fac655
Sandeep Jadam <withsj.in@gmail.com>
2023-08-26 17:14:14 +0530

    Update Python CSQ AST

--- AST/ast.py
+++ AST/ast.py
@@ -3,24 +3,25 @@ Python implementation of Csq AST
 '''
 
 #Node Types 
-EXPR = 0
-VAR_DECL = 1
-VAR_ASSIGN = 2
-FUN_DECL = 3
-IF_STMT = 4
-ELSE_STMT = 5
-ELIF_STMT = 6
-FOR_STMT = 7
-WHILE_STMT = 8
-BLOCK = 9
-FUN_CALL = 10 
-PRINT = 11
-TYPE = 12
-ACCESS = 13
-COLLECTION_UPDATE = 14
-RETURN = 15
-IMPORT = 16
-UNKNOWN_NODE = 17
+class NodeTypes:
+    EXPR = 0
+    VAR_DECL = 1
+    VAR_ASSIGN = 2
+    FUN_DECL = 3
+    IF_STMT = 4
+    ELSE_STMT = 5
+    ELIF_STMT = 6
+    FOR_STMT = 7
+    WHILE_STMT = 8
+    BLOCK = 9
+    FUN_CALL = 10 
+    PRINT = 11
+    TYPE = 12
+    ACCESS = 13
+    COLLECTION_UPDATE = 14
+    RETURN = 15
+    IMPORT = 16
+    UNKNOWN_NODE = 17
     
 #Parent AST node type
 
@@ -33,7 +34,7 @@ class ExprNode(ASTNode):
     def __init__(self):
         super().__init__()
         self.tokens = []
-        self.type = EXPR
+        self.type = NodeTypes.EXPR
 
 class VarDeclNode(ASTNode):
     def __init__(self):
@@ -41,44 +42,44 @@ class VarDeclNode(ASTNode):
         self.identifier = ""
         self.var_type = ""
         self.value = None
-        self.type = VAR_DECL
+        self.type = NodeTypes.VAR_DECL
 
 class VarAssignNode(ASTNode):
     def __init__(self):
         super().__init__()
         self.identifier = ""
         self.value = None
-        self.type = VAR_ASSIGN
+        self.type = NodeTypes.VAR_ASSIGN
 
 class BlockNode(ASTNode):
     def __init__(self):
         super().__init__()
         self.statements = []
-        self.type = BLOCK
+        self.type = NodeTypes.BLOCK
 
 class FunDeclNode(ASTNode):
     def __init__(self):
         super().__init__()
         self.identifier = ""
         self.parameters = []
-        self.type = FUN_DECL
+        self.type = NodeTypes.FUN_DECL
 
 class IfStmtNode(ASTNode):
     def __init__(self):
         super().__init__()
         self.condition = None
-        self.type = IF_STMT
+        self.type = NodeTypes.IF_STMT
 
 class ElseStmtNode(ASTNode):
     def __init__(self):
         super().__init__()
-        self.type = ELSE_STMT
+        self.type = NodeTypes.ELSE_STMT
 
 class ElifStmtNode(ASTNode):
     def __init__(self):
         super().__init__()
         self.condition = None
-        self.type = ELIF_STMT
+        self.type = NodeTypes.ELIF_STMT
 
 class ForStmtNode(ASTNode):
     def __init__(self):
@@ -86,43 +87,43 @@ class ForStmtNode(ASTNode):
         self.body = None
         self.iter_name = ""
         self.condition = None
-        self.type = FOR_STMT
+        self.type = NodeTypes.FOR_STMT
 
 class WhileStmtNode(ASTNode):
     def __init__(self):
         super().__init__()
         self.condition = None
-        self.type = WHILE_STMT
+        self.type = NodeTypes.WHILE_STMT
 
 class CallNode(ASTNode):
     def __init__(self):
         super().__init__()
         self.name = ""
         self.params = []
-        self.type = FUN_CALL
+        self.type = NodeTypes.FUN_CALL
 
 class PrintNode(ASTNode):
     def __init__(self):
         super().__init__()
         self.value = None
-        self.type = PRINT
+        self.type = NodeTypes.PRINT
 
 class TypeNode(ASTNode):
     def __init__(self):
         super().__init__()
-        self.type = TYPE
+        self.type = NodeTypes.TYPE
 
 class UnknownNode(ASTNode):
     def __init__(self):
         super().__init__()
-        self.type = UNKNOWN_NODE
+        self.type = NodeTypes.UNKNOWN_NODE
 
 class AccessNode(ASTNode):
     def __init__(self):
         super().__init__()
         self.index = 0
         self.source = ""
-        self.type = ACCESS
+        self.type = NodeTypes.ACCESS
 
 class CollectionUpdateNode(ASTNode):
     def __init__(self):
@@ -130,16 +131,16 @@ class CollectionUpdateNode(ASTNode):
         self.index = None
         self.source = ""
         self.value = None
-        self.type = COLLECTION_UPDATE
+        self.type = NodeTypes.COLLECTION_UPDATE
 
 class ReturnNode(ASTNode):
     def __init__(self):
         super().__init__()
         self.value = None
-        self.type = RETURN
+        self.type = NodeTypes.RETURN
 
 class ImportNode(ASTNode):
     def __init__(self):
         super().__init__()
         self.name = ""
-        self.type = IMPORT
+        self.type = NodeTypes.IMPORT
