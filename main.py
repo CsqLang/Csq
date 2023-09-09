fe1ee827b61c2b69d2506290e8cc316d644280d2
Sandeep Jadam <withsj.in@gmail.com>
2023-09-09 23:34:52 +0530

    main.py update tokens create

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-from Tokenizer.tokenizer import *
-from Parser.parser import *
+from Runtime.code_format import toTokens,readCode,writeCode
+from Parser.parser import Compile
+import sys 
+
+arguments = sys.argv
+filename = str()
+filepath = str()
+if len(arguments) == 3:
+    filename = arguments[1]
+    filepath = arguments[2]
+else:
+    print("Argv[Filename.csq filefolder]")
+    exit()
+
+code = readCode(filepath+"/"+filename)
+
+
+Token_code = toTokens(code=code)
+
+for token_line in Token_code: # This code for testing Token
+    for token in token_line:
+        print(token)          
+
+# compile_code = Compile(Token_code)
