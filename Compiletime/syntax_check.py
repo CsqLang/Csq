a2b4f45bfe16da55590adc6dba615ee68b1f772b
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-09-11 19:38:31 +0530

    Update syntax_check.py

--- Compiletime/syntax_check.py
+++ Compiletime/syntax_check.py
@@ -21,3 +21,21 @@ def check_VarDecl(tokens):
         else:
             valid = 0
     return valid
+
+def check_VarAssign(tokens):
+    '''
+    Format:
+    <identifier> = <Expr>
+    '''
+    valid = 0
+    if tokens[0].type == TokenType.IDENTIFIER:
+        valid = 1
+
+        if tokens[1].token == '=':
+            for i in tokens[1::]:
+                if i.type == TokenType.KEYWORD:
+                    valid = 0
+                    break
+        else:
+            valid = 0
+    return valid
