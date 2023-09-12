81400b90881773f13b53fad0298578b782acc816
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-09-12 16:23:48 +0530

    Update syntax_check.py

--- Compiletime/syntax_check.py
+++ Compiletime/syntax_check.py
@@ -1,21 +1,19 @@
-"""
+'''
 THis will be checking for syntax errors where grammar is voilated.
-"""
-from Compiletime.error import (Error, IndentationError, NameError, SyntaxError,
-                               TypeError)
+'''
 from Tokenizer.tokenizer import TokenType
-
+from Compiletime.error import TypeError, IndentationError, NameError, SyntaxError, Error
 
 def check_VarDecl(tokens):
-    """
+    '''
     Format:
     <identifier> := <Expr>
-    """
+    '''
     valid = False
     if tokens[0].type == TokenType.IDENTIFIER:
         valid = True
 
-        if tokens[1].token == ":=":
+        if tokens[1].token == ':=':
             for i in tokens[1:]:
                 if i.type == TokenType.KEYWORD:
                     valid = False
@@ -24,21 +22,35 @@ def check_VarDecl(tokens):
             valid = False
     return valid
 
-
 def check_VarAssign(tokens):
-    """
+    '''
     Format:
     <identifier> = <Expr>
-    """
+    '''
     valid = False
     if tokens[0].type == TokenType.IDENTIFIER:
         valid = True
 
-        if tokens[1].token == "=":
+        if tokens[1].token == '=':
             for i in tokens[1:]:
                 if i.type == TokenType.KEYWORD:
                     valid = False
                     break
         else:
             valid = False
+    return valid
+
+def check_PrintStmt(tokens):
+    '''
+    Format:
+    print <Expr>
+    '''
+    valid = True
+
+    for i in tokens[1:]:
+        if i.token == ":=" or i.token == "=":
+            valid = False
+        elif i.type == TokenType.KEYWORD:
+            valid = False
+
     return valid
