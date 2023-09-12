a5d46ca0729b03df8e5bde4ecf55e3af41002eec
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-09-12 08:02:00 +0530

    Create wrapper.py

---
+++ Compiletime/wrapper.py
@@ -0,0 +1,7 @@
+'''
+This file binds the code parsed and visited with C/C++ APIs
+'''
+def bind(current_path, code):
+    res = '#include "' + current_path + '/Builtin/basic.h"\n'
+    res += 'int main(){\n' + code + '\n return 0;\n}\n'
+    return res
