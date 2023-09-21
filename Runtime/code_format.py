52d6ab4294c43585aeb4cfcb5077f46b17937dc0
Sandeep Jadam <withsj.in@gmail.com>
2023-09-21 21:50:36 +0530

    code_format.py update code doc

--- Runtime/code_format.py
+++ Runtime/code_format.py
@@ -3,7 +3,15 @@ from Tokenizer.tokenizer import *
 
 
 def readCode(path: str) -> str:
-    """Read Csq Code File"""
+    """
+    Read Csq code from a file.
+
+    Args:
+        path (str): The path to the Csq code file.
+
+    Returns:
+        str: The content of the code file as a string.
+    """
     code = str()
     with open(path) as codefile:
         for line in codefile.readlines():
@@ -15,17 +23,16 @@ def readCode(path: str) -> str:
     return code
 
 
-# Not need of split() in python
-# def split(string:str) -> list:
-#     tokens = list()
-
-#     for line in string.split("\n"):
-#         tokens.append(line)
-
-#     return tokens
-
-
 def toTokens(code: str) -> list:
+    """
+    Tokenize Csq code into a list of token streams.
+
+    Args:
+        code (str): The Csq code as a string.
+
+    Returns:
+        list: A list of token streams, where each stream is a list of tokens.
+    """
     tokens = list()
     statements = code.split("\n")
 
@@ -41,5 +48,15 @@ def toTokens(code: str) -> list:
 
 
 def writeCode(code: str, path: str) -> None:
+    """
+    Write Csq code to a file.
+
+    Args:
+        code (str): The Csq code as a string.
+        path (str): The path to the output file where the code will be written.
+
+    Returns:
+        None
+    """
     with open(path, "w") as codefile:
         codefile.write(code)
