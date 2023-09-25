af18ba16310523cd1ef2fcce9dcd1cc6d68fa8ba
Sandeep Jadam <withsj.in@gmail.com>
2023-09-26 00:11:35 +0530

    for install use build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,57 @@
+#!/bin/bash
+
 # The terminal should be refreshed for effects to take place
-# Can be witten better, but this is just a concept
+# Can be written better, but this is just a concept
 
 # Copy it to ~/.local/bin for it to be global
-
 local_bin="$HOME/.local/bin/"
+cp csq.py "$local_bin"
 
-# Copy the include directories to ~/.local/include for global
-local_include="$HOME/.local/include"
-r_s=$HOME/.${SHELL##*/}rc
+if [ $? -eq 0 ]; then
+  # Destination folder for include files
+  local_include="$HOME/.local/include/csq"
 
-# Check if CSQ_PATH is already set in .bashrc
-if ! grep -q "CSQ_INCLUDE" "$r_s"; then
-  # Append the current directory to PATH in .bashrc
-  CSQ_BIN="$local_bin/csq"
-  CSQ_PATH="$local_include/csq"
-  echo 'export PATH="'"$CSQ_BIN"':$PATH"' >>"$r_s"
-  echo 'export CSQ_INCLUDE="'"$CSQ_PATH"'"' >>"$r_s"
-  # Apply the changes in the current shell
-  source "$r_s"
+  # Create the destination folder if it doesn't exist
+  mkdir -p "$local_include"
+
+  if [ $? -eq 0 ]; then
+    # Copy all subdirectories from the current folder to the destination folder
+    for dir in */; do
+      # Exclude the csq directory itself if present
+      if [ "$dir" != "csq/" ]; then
+        cp -r "$dir" "$local_include"
+      fi
+    done
+
+    if [ $? -eq 0 ]; then
+      # Check if CSQ_PATH is already set in your shell configuration file (e.g., .bashrc)
+      shell_config="$HOME/.${SHELL##*/}rc"
+
+      if grep -q "CSQ_INCLUDE" "$shell_config"; then
+        echo -e "\033[1;33m[WARNING]\033[0m CSQ is already set up in your shell configuration."
+        echo
+        echo "🚀 Csq has been successfully installed! Enjoy coding with Csq!"
+      else
+        # Append the current directory to PATH in your shell configuration file
+        CSQ_BIN="$local_bin/csq"
+        CSQ_PATH="$local_include"
+        echo 'export PATH="'"$CSQ_BIN"':$PATH"' >> "$shell_config"
+        echo 'export CSQ_INCLUDE="'"$CSQ_PATH"'"' >> "$shell_config"
+        # Apply the changes in the current shell
+        source "$shell_config"
+
+        if [ $? -eq 0 ]; then
+          echo "🚀 Csq has been successfully installed! Enjoy coding with Csq!"
+        else
+          echo "Error: Failed to apply changes in your shell configuration. Please manually source $shell_config."
+        fi
+      fi
+    else
+      echo "Error: Failed to copy subdirectories to $local_include."
+    fi
+  else
+    echo "Error: Failed to create directory $local_include."
+  fi
+else
+  echo "Error: Failed to copy csq.py to $local_bin."
 fi
