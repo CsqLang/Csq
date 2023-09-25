9911bfd0876901ed36cc40be3f3cc2549ece7773
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-09-25 19:48:01 +0530

    Create codes.h

---
+++ Builtin/codes.h
@@ -0,0 +1,19 @@
+/*
+
+This file contains IR codes for different CsqAPI's Runtime functions
+
+Why do we need it?
+When C++ code is produced it's often big and quite difficult
+to understand to remove this complexity here we will be providing
+short names which looks elegant.
+
+*/
+#include "../Runtime/memory.h"
+#include "../Runtime/core.h"
+#include <iostream>
+
+/****************************************/
+
+#define main int main(int argc, char** argv){
+#define endmain return 0;}
+
