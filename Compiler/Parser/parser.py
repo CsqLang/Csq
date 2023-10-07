3fc2edc44bb3a59cad90705c3579c40c1ff1ba8b
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-10-07 18:34:20 +0530

    Update parser.py

--- Compiler/Parser/parser.py
+++ Compiler/Parser/parser.py
@@ -245,7 +245,6 @@ def parse_ExprNode(tokens) -> ExprNode:
                 node.tokens.append(Token(current_token.token + "(", TokenType.BLANK))
                 i += 1
             elif i + 1 < len(tokens) and tokens[i + 1].token == ".":
-                print(4)
                 node.tokens.append(Token(f'methodId("{current_token.token}","{tokens[i+2].token}")', TokenType.BLANK))
                 i += 2
             else:
