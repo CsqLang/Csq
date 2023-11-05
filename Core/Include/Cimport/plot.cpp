d406f8ce171aaf410e4e2fda37a30c85a7d2c16d
AniketKumar2500 <geek.aniket.coder@gmail.com>
2023-11-05 10:15:57 +0530

    Added a scatter function

--- Core/Include/Cimport/plot.cpp
+++ Core/Include/Cimport/plot.cpp
@@ -35,3 +35,38 @@ auto plot=[&](Cell x, Cell y){
     string command = "python3 -c \"" + code + "\"";
     int r = system(command.c_str());
 };
+
+auto scatter=[&](Cell x, Cell y){
+    string x_arr = "[";
+    string y_arr = "[";
+    
+    for(Cell e : *(x.vectorVal)){
+        switch(e.type){
+            case Type::INT:{
+                x_arr += to_string(e.intVal) + ",";
+            }
+            case Type::FLOAT:{
+                x_arr += to_string(e.floatVal) + ",";
+            }
+        }
+    }
+    x_arr.pop_back();
+    x_arr += "]";
+
+    for(Cell e : *(y.vectorVal)){
+        switch(e.type){
+            case Type::INT:{
+                y_arr += to_string(e.intVal) + ",";
+            }
+            case Type::FLOAT:{
+                y_arr += to_string(e.floatVal) + ",";
+            }
+        }
+    }
+    y_arr.pop_back();
+    y_arr += "]";
+    // cout << x_arr << endl;
+    string code = "import matplotlib.pyplot as plt;plt.scatter(" + x_arr + "," + y_arr + ");plt.show();";
+    string command = "python3 -c \"" + code + "\"";
+    int r = system(command.c_str());
+};
