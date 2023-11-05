a43c32a73e4cc6c341524d619ab6a8561b29eb6a
AniketKumar2500 <geek.aniket.coder@gmail.com>
2023-11-05 10:11:09 +0530

    Adding a plot module in c++ for plotting graphs

---
+++ Core/Include/Cimport/plot.cpp
@@ -0,0 +1,37 @@
+#include <Csq/Core/Runtime/memory.h>
+#include <Csq/Core/Runtime/core.h>
+
+auto plot=[&](Cell x, Cell y){
+    string x_arr = "[";
+    string y_arr = "[";
+    
+    for(Cell e : *(x.vectorVal)){
+        switch(e.type){
+            case Type::INT:{
+                x_arr += to_string(e.intVal) + ",";
+            }
+            case Type::FLOAT:{
+                x_arr += to_string(e.floatVal) + ",";
+            }
+        }
+    }
+    x_arr.pop_back();
+    x_arr += "]";
+
+    for(Cell e : *(y.vectorVal)){
+        switch(e.type){
+            case Type::INT:{
+                y_arr += to_string(e.intVal) + ",";
+            }
+            case Type::FLOAT:{
+                y_arr += to_string(e.floatVal) + ",";
+            }
+        }
+    }
+    y_arr.pop_back();
+    y_arr += "]";
+    // cout << x_arr << endl;
+    string code = "import matplotlib.pyplot as plt;plt.plot(" + x_arr + "," + y_arr + ");plt.show();";
+    string command = "python3 -c \"" + code + "\"";
+    int r = system(command.c_str());
+};
