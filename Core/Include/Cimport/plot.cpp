f7933bf11eaab8957f470a75194ed363320c94f3
AniketKumar2500 <geek.aniket.coder@gmail.com>
2023-11-05 21:52:12 +0530

    Added bar, pie functions for plotting

--- Core/Include/Cimport/plot.cpp
+++ Core/Include/Cimport/plot.cpp
@@ -9,9 +9,11 @@ auto plot=[&](Cell x, Cell y){
         switch(e.type){
             case Type::INT:{
                 x_arr += to_string(e.intVal) + ",";
+                break;
             }
             case Type::FLOAT:{
                 x_arr += to_string(e.floatVal) + ",";
+                break;
             }
         }
     }
@@ -22,9 +24,11 @@ auto plot=[&](Cell x, Cell y){
         switch(e.type){
             case Type::INT:{
                 y_arr += to_string(e.intVal) + ",";
+                break;
             }
             case Type::FLOAT:{
                 y_arr += to_string(e.floatVal) + ",";
+                break;
             }
         }
     }
@@ -44,9 +48,11 @@ auto scatter=[&](Cell x, Cell y){
         switch(e.type){
             case Type::INT:{
                 x_arr += to_string(e.intVal) + ",";
+                break;
             }
             case Type::FLOAT:{
                 x_arr += to_string(e.floatVal) + ",";
+                break;
             }
         }
     }
@@ -57,9 +63,11 @@ auto scatter=[&](Cell x, Cell y){
         switch(e.type){
             case Type::INT:{
                 y_arr += to_string(e.intVal) + ",";
+                break;
             }
             case Type::FLOAT:{
                 y_arr += to_string(e.floatVal) + ",";
+                break;
             }
         }
     }
@@ -67,6 +75,65 @@ auto scatter=[&](Cell x, Cell y){
     y_arr += "]";
     // cout << x_arr << endl;
     string code = "import matplotlib.pyplot as plt;plt.scatter(" + x_arr + "," + y_arr + ");plt.show();";
+    string command = "python3 -c \"" + code + "\"";
+    int r = system(command.c_str());
+};
+
+auto bar=[&](Cell x, Cell y){
+    string x_arr = "[";
+    string y_arr = "[";
+    
+    for(Cell e : *(x.vectorVal)){
+            x_arr += (*e.stringVal) + ",";
+    }
+    x_arr.pop_back();
+    x_arr += "]";
+
+    for(Cell e : *(y.vectorVal)){
+        switch(e.type){
+            case Type::INT:{
+                y_arr += to_string(e.intVal) + ",";
+                break;
+            }
+            case Type::FLOAT:{
+                y_arr += to_string(e.floatVal) + ",";
+                break;
+            }
+        }
+    }
+    y_arr.pop_back();
+    y_arr += "]";
+    
+    string code = "import matplotlib.pyplot as plt;plt.bar(" + x_arr + "," + y_arr + ");plt.show();";
+    string command = "python3 -c \"" + code + "\"";
+    int r = system(command.c_str());
+};
+auto pie=[&](Cell x, Cell y){
+    string x_arr = "[";
+    string y_arr = "[";
+    
+    for(Cell e : *(x.vectorVal)){
+            x_arr += (*e.stringVal) + ",";
+    }
+    x_arr.pop_back();
+    x_arr += "]";
+
+    for(Cell e : *(y.vectorVal)){
+        switch(e.type){
+            case Type::INT:{
+                y_arr += to_string(e.intVal) + ",";
+                break;
+            }
+            case Type::FLOAT:{
+                y_arr += to_string(e.floatVal) + ",";
+                break;
+            }
+        }
+    }
+    y_arr.pop_back();
+    y_arr += "]";
+    
+    string code = "import matplotlib.pyplot as plt;plt.pie(" + y_arr + ",labels = " + x_arr + ");plt.show();";
     string command = "python3 -c \"" + code + "\"";
     int r = system(command.c_str());
 };
