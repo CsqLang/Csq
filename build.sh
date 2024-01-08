d07cd6d2b8e2e7fc846d3c0f75ed6cab1d953cf1
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2024-01-08 15:13:36 +0300

    Rewrite a bash script (#274)
    * Update csq.py
    
    * Update memory.h
    
    * chore: Rewrite the installation script
    
    * chore: Use argparse and sync csq.py with bash script
    
    ---------
    
    Signed-off-by: Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
    Co-authored-by: Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,127 +1,116 @@
-#!/bin/bash
+#!/bin/env bash
 
-# Installation path
-local_bin="$HOME/.local/bin/"
-local_include="$HOME/.local/include/csq"
-csq_bin="$local_bin/csq"
-csq_path="$local_include"
+# Check if the user is root (or at least has the necessary permissions)
+# This doesn't affect the execution, it just checks other alternatives to install the files to
+# Check this comment about permissions (and the whole question by extension)
+# https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script#comment125628861_52586842
+# The test can also be "faked" by fakeroot, so let the user run the script
+# If the user is not root, append 'sudo' to the commands that require it.
 
-if [ -f "$HOME/.bashrc" ]; then
-    shell_config="$HOME/.bashrc"
-else
-    shell_config="$HOME/.${SHELL##*/}rc"
-fi
+# The following locations are to be used:
+# 	$HOME/.csq/include/csq
+# 	/usr/local/include/csq
+# 	/opt/csq
+# 	/usr/include/csq
+# 	~/.local/bin
+# 	~/.local/lib/csq
+# 	~/.local/share/csq
 
+# Start from the least permission requirement location
 
-function install_csq() {
-  # Copy csq.py to ~/.local/bin/
-  cp csq.py csq
-  chmod +x csq
-  mv csq "$local_bin"
-  sudo cp -r Core "/usr/include/Csq/"
-  if [ $? -ne 0 ]; then
-    echo "Error: Failed to copy csq.py to $local_bin."
-    exit 1
-  fi
+is_root() {
+    [ "${EUID:-$(id -u)}" -eq 0 ]
+}
 
-  # Create destination folder ~/.local/include/csq
-  mkdir -p "$local_include"
-  if [ $? -ne 0 ]; then
-    echo "Error: Failed to create directory $local_include."
-    exit 1
-  fi
+can_write() {
+    [ -w "$1" ]
+}
 
-  # Copy specific folders to ~/.local/include/csq
-  cp -r Compiler "$local_bin"
-  cp -r Core "$local_include"
-
-  if [ $? -ne 0 ]; then
-    echo "Error: Failed to copy specific folders to $local_include."
-    exit 1
-  fi
-
-  # Check if CSQ_INCLUDE is already set in the shell configuration file
-
-  if grep -q "CSQ_INCLUDE" "$shell_config"; then
-    echo -e "\033[1;33m[WARNING]\033[0m CSQ is already set up in your shell configuration."
-    echo
-    echo "🚀 Csq has been successfully installed! Enjoy coding with Csq!"
-  else
-    # Append the current directory to PATH and CSQ_INCLUDE to the shell configuration file
-    echo 'export PATH="'"$csq_bin"':$PATH"' >> "$shell_config"
-    echo 'export CSQ_INCLUDE="'"$csq_path"'"' >> "$shell_config"
-    # Apply changes in the current shell
-    source "$shell_config"
-
-    if [ $? -eq 0 ]; then
-      echo "🚀 Csq has been successfully installed! Enjoy coding with Csq!"
+function install() {
+	# Make the csq.py executable before doing anything
+	chmod +x csq.py
+    if is_root || can_write "/opt"; then
+        echo "I'll install csq in /opt/csq and create a symlink to /usr/local/bin/csq"
+        mkdir -p /opt/csq
+        cp -r Core Compiler "/opt/csq"
+        cp csq.py "/opt/csq"
+        ln -sf "/opt/csq/csq.py" "/usr/local/bin/csq"
     else
-      echo "Error: Failed to apply changes in your shell configuration. Please manually source $shell_config."
-      exit 1
+        # Check for other locations to install
+
+        if can_write "$HOME"; then
+            echo "I'll install csq in $HOME/.csq/include/csq and create a symlink to ~/.local/bin/csq"
+            mkdir -p "$HOME/.csq/include/csq"
+            cp -r Core Compiler "$HOME/.csq/include/csq"
+            cp csq.py "$HOME/.csq/include/csq"
+            ln -sf "$HOME/.csq/include/csq/csq.py" "$HOME/.local/bin/csq"
+        elif can_write "/usr/local/include"; then
+            echo "I'll install csq in /usr/local/include/csq and create a symlink to /usr/local/bin/csq"
+            mkdir -p /usr/local/include/csq
+            cp -r Core Compiler "/usr/local/include/csq"
+            cp csq.py "/usr/local/include/csq"
+            ln -sf "/usr/local/include/csq/csq.py" "/usr/local/bin/csq"
+        elif can_write "/usr/include"; then
+            echo "I'll install csq in /usr/include/csq and create a symlink to /usr/bin/csq"
+            mkdir -p /usr/include/csq
+            cp -r Core Compiler "/usr/include/csq"
+            cp csq.py "/usr/include/csq"
+            ln -sf "/usr/include/csq/csq.py" "/usr/bin/csq"
+        elif can_write "$HOME/.local/bin"; then
+            echo "I'll install csq in ~/.local/bin and create a symlink to ~/.local/bin/csq"
+            cp csq.py "$HOME/.local/bin/csq"
+        elif can_write "$HOME/.local/lib"; then
+            echo "I'll install csq in ~/.local/lib/csq"
+            mkdir -p "$HOME/.local/lib/csq"
+            cp -r Core Compiler "$HOME/.local/lib/csq"
+            cp csq.py "$HOME/.local/lib/csq"
+        elif can_write "$HOME/.local/share"; then
+            echo "I'll install csq in ~/.local/share/csq"
+            mkdir -p "$HOME/.local/share/csq"
+            cp -r Core Compiler "$HOME/.local/share/csq"
+            cp csq.py "$HOME/.local/share/csq"
+        else
+            echo "No writable location found. Please install manually"
+        fi
     fi
-  fi
 }
 
-function uninstall_csq() {
-  # Remove csq.py from ~/.local/bin/
-  rm -f "$csq_bin"
+function uninstall() {
+    # Look for the places to uninstall
+    dirs=(
+        "$HOME/.csq/include/csq"
+        "/usr/local/include/csq"
+        "/opt/csq"
+        "/usr/include/csq"
+        "$HOME/.local/share/csq"
+        "$HOME/.local/lib/csq"
+    )
 
-  # Remove specific folders from ~/.local/include/csq
-  rm -rf "$local_bin/Compiler"
-  rm -rf "$csq_path/Core"
+    bins=(
+        "/usr/local/bin/csq"
+        "$HOME/.local/bin/csq"
+    )
 
-  # Remove CSQ_INCLUDE from the shell configuration file
-  #sed -i '/CSQ_INCLUDE/d' "$shell_config"
+    for dir in "${dirs[@]}"; do
+        if [ -d "$dir" ]; then
+            rm -rf "$dir"
+            echo "Uninstalled from $dir"
+        fi
+    done
 
-  #echo "✅ Csq has been successfully uninstalled."
-
-
-  # Check if the shell configuration file exists before attempting to modify it
-  if [ -f "$shell_config" ]; then
-    # Remove CSQ_INCLUDE from the shell configuration file
-    sed -i '/CSQ_INCLUDE/d' "$shell_config"
-    echo "✅ Csq has been successfully uninstalled."
-  else
-    echo "Error: Shell configuration file $shell_config not found."
-    exit 1
-  fi
-
-
+    for bin in "${bins[@]}"; do
+        if [ -L "$bin" ]; then
+            unlink "$bin"
+            echo "Unlink binary from $bin"
+        fi
+    done
 }
 
-function uninstall_csq() {
-  # Remove csq.py from ~/.local/bin/
-  rm -f "$csq_bin"
-
-  # Remove specific folders from ~/.local/include/csq
-  rm -rf "$local_bin/Compiler"
-  rm -rf "$csq_path/Core"
-
-  # Remove CSQ_INCLUDE from the shell configuration file
-  if [ -f "$shell_config" ]; then
-    sed -i '/CSQ_INCLUDE/d' "$shell_config"
-    echo "✅ CSQ_INCLUDE removed from $shell_config."
-  else
-    echo "Error: Shell configuration file $shell_config not found."
-    exit 1
-  fi
-
-  # Remove csq path from the shell configuration file
-  if [ -f "$shell_config" ]; then
-    sed -i '/csq/d' "$shell_config"
-    echo "✅ CSQ_PATH removed from $shell_config."
-  else
-    echo "Error: .bashrc file not found."
-    exit 1
-  fi
-
-  echo "✅ Csq has been successfully uninstalled."
-}
-
-
-# Check command-line argument
-if [ "$1" == "uninstall" ]; then
-  uninstall_csq
+if [ "$1" == "install" ] || [ "$1" == "-i" ]; then
+	install
+elif [ "$1" == "uninstall" ] || [ "$1" == "-u" ]; then
+	uninstall
 else
-  install_csq
+	echo "Usage: build.sh <install|uninstall>"
 fi
+
